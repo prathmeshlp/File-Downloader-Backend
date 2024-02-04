e43261ff8722b704d9269567d1f547e98f224814
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2024-02-04 11:57:08 +0530

    Added validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required:true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required:true,
+    unique: true
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
